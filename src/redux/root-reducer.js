998857e46d21d5ff1511eb87d53678cc000c971d
Ritesh <ritesh.aws_study@outlook.com>
2021-01-02 16:20:18 +1100

    moved directory and shop data into reducers, with selectors,  and made individual routes to all the sections of the front page

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,24 @@
 import { combineReducers } from "redux";
-import userReducer from "./user/user.reducer";
-import CartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// importing local storage to use as default storage
 
-export default combineReducers({
+import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+
+const persistconfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart"], //whitelist --takes array of string to store the reducer
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
-	cart: CartReducer,
+	cart: cartReducer,
+	directory: directoryReducer,
+	shop: shopReducer,
 });
+
+export default persistReducer(persistconfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
@@ -7,5 +8,6 @@ import rootReducer from "./root-reducer";
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
